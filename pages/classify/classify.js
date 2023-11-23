b330d5e8a522250c0e447561eb57399c181f4cd7
buildkay <2803450297@qq.com>
2023-11-23 22:54:50 +0800

    产品分类页完成

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -1,14 +1,76 @@
-// pages/classify/classify.js
+import { listNav, queryProduct } from "../../api/apis";
+let navid;
 Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    navActive: 0,
+    navArr: [],
+    proArr: [],
+    loading: false,
+    isData: false,
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {},
+  async onLoad(options) {
+    let { idx } = options;
+    await this.getNavList();
+    if (idx) {
+      this.navChange(idx);
+    } else {
+      navid = this.data.navArr[0]._id;
+      this.getProductList();
+    }
+  },
+
+  //获取分类导航
+  async getNavList() {
+    await listNav().then((res) => {
+      this.setData({
+        navArr: res.data,
+      });
+      this.selectComponent("#myTabs").resize();
+    });
+  },
+
+  //获取产品列表
+  getProductList(s = 0) {
+    this.setData({
+      loading: true,
+    });
+    queryProduct({
+      navid: navid,
+      size: s,
+    }).then((res) => {
+      let oldArr = this.data.proArr;
+      let newArr = oldArr.concat(res.data);
+      this.setData({
+        proArr: newArr,
+        loading: false,
+      });
+      if (res.total == this.data.proArr.length) {
+        this.setData({
+          isData: true,
+        });
+      }
+    });
+  },
+
+  //导航条切换事件
+  navChange(e) {
+    let index = e?.detail?.index ?? e;
+    navid = this.data.navArr[index]._id;
+    this.setData({
+      proArr: [],
+      loading: false,
+      isData: false,
+      navActive: Number(index),
+    });
+    this.getProductList();
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -38,7 +100,10 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {},
+  onReachBottom() {
+    if (this.data.isData) return;
+    this.getProductList(this.data.proArr.length);
+  },
 
   /**
    * 用户点击右上角分享
